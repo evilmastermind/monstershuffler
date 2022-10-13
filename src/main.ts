dfef9033bdb037df722db25fde08df211e866a33
Ismael <thenight99@gmail.com>
2022-10-13 20:31:04 +0200

    environment setup -----------

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "@/App.vue";
+import { createI18n } from "vue-i18n";
+import en from "@/locales/en.json";
+import it from "@/locales/it.json";
 
-createApp(App).mount('#app')
+type MessageSchema = typeof en; 
+const i18n = createI18n<[MessageSchema], "en" | "it">({
+  locale: "en",
+  messages: {
+    en: en,
+    it: it,
+  }
+});
+
+const app = createApp(App);
+const pinia = createPinia();
+app
+  .use(pinia)
+  .use(i18n)
+  .mount("#app")
+;
