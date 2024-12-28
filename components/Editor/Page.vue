b462c2bf1e2c0ff9b8d6512259f2dcb2683d52cd
Ismaele Gobbo <ismaele.gobbo@silla.industries>
2024-12-28 08:59:01 +0100

    backup

--- components/Editor/Page.vue
+++ components/Editor/Page.vue
@@ -1,6 +1,11 @@
 <template>
   <NavbarPadding />
-  <div>Page.</div>
+  <div class="lg-max">
+    <ul class="content">
+      <li>the search bar</li>
+      <li>note groups</li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
